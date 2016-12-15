1fc3f6ffbc52d0f157f44fe9314efd338fdc9b97
Rhyd Lewis <rhyd.lewis@icloud.com>
2016-12-15 16:42:53 +0000

    added alias

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -14,3 +14,4 @@ alias pip-upgrade-all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xa
 alias p='ps aux| grep -v grep| grep -i'
 alias ebp='slime ~/.bash_profile'
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; rbenv rehash;'
+alias edlast='open `ls -tr | tail -1`'
